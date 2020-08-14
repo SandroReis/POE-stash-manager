6233c978a8420a799b3bf014cb48c0a94bde796a
Sandro Reis <sandrouh@hotmail.com>
2020-08-13 22:29:14 -0300

    feat: :sparkles: adding a landingPage

---
+++ renderer/src/pages/Landing/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './styles.css'
+
+export default function Landing () {
+  return (
+    <div id="page-landing">
+      <h1>POE Stash Manager</h1>
+      <div className="content-wrapper">
+        <p>tell me your price ...</p>
+      </div>
+      <div className="button-wrapper">
+        <button type="button" className="primary-button">Get start!</button>
+      </div>
+    </div>
+  )
+}
